eefd629e2ef76b0437b36b04b7bbaac1cdc6f8a2
Nhlanhla <nfortunengcobo@gmail.com>
2025-04-03 02:00:35 +0200

    Added New Endpoint

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 # flask_microservice.py
 from flask import Flask, request, jsonify
 from transformers import pipeline
-from pydantic import BaseModel
 
+import spacy
+from nltk.sentiment import SentimentIntensityAnalyzer
+import nltk
 
 app = Flask(__name__)
 
-# Load sentiment analysis model
+# Load sentiment analysis model PyTorch
 sentiment_pipeline = pipeline("sentiment-analysis")
 
+# Load sentiment analysis model VADER
+nltk.download("vader_lexicon")
+sia = SentimentIntensityAnalyzer()
+
+
+#not used at the moment
 @app.route('/process', methods=['POST'])
 def process():
     data = request.get_json()
@@ -27,17 +35,30 @@ def process_data(input_data):
     except Exception as e:
         return {'success': False, 'error': str(e)}
 
+
+# Sentiment Analysis using PyTorch
 @app.route('/analyze', methods=['POST'])
 def analyze():
     data = request.get_json()
-    input_data = data.get('text')
-    return analyze_sentiment(input_data)
+    input_data = data.get('Text')
+    return jsonify(analyze_sentiment(input_data))
 
 def analyze_sentiment(input_data):
     result = sentiment_pipeline(input_data)
     return result
     #return {'success': False, 'error': 'Just a test endpoint'}
 
+#Sentiment Analysis using Vader
+@app.route('/analyze-vader', methods=['POST'])
+def analyze_vader():
+    data = request.get_json()
+    input_data = data.get('Text')
+    return jsonify(analyze_sentiment_vader(input_data))
+
+def analyze_sentiment_vader(input_data):
+    result = sia.polarity_scores(input_data)
+    return result
+    # return {'success': False, 'error': 'Just a test endpoint'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
